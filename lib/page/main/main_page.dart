73ad30544b3aca5f5d663ff5a72b9a13928aa4cc
seferovramin7 <seferovramin7@gmail.com>
2025-02-15 14:40:14 +0400

    payment screen

--- lib/page/main/main_page.dart
+++ lib/page/main/main_page.dart
@@ -2,6 +2,7 @@ import 'package:flutter/material.dart';
 import 'package:green_public_mobile/page/main/widget/main_app_bar.dart';
 import 'package:green_public_mobile/page/main/widget/main_body.dart';
 import 'package:green_public_mobile/page/main/widget/main_bottom_navigation_bar.dart';
+import 'package:green_public_mobile/page/payment/paymentPage.dart';
 
 class MainPage extends StatefulWidget {
   const MainPage({super.key});
@@ -18,6 +19,18 @@ class _MainPageState extends State<MainPage>  with SingleTickerProviderStateMixi
       child: MainAppBar()),
       body: const MainBody(),
       bottomNavigationBar: MainBottomNavigationBar(currentPageIndex: 0,),
+      floatingActionButton: FloatingActionButton(
+        onPressed: () {
+          Navigator.of(context).push(
+            MaterialPageRoute(
+              builder: (context) => const PaymentPage(
+                paymentUrl: 'https://checkout-preprod.cibpay.co/pay/91532085758869474',
+              ),
+            ),
+          );
+        },
+        child: const Icon(Icons.payment),
+      ),
     );
   }
 }
---
+++ lib/page/payment/paymentPage.dart
@@ -0,0 +1,53 @@
+import 'package:flutter/material.dart';
+import 'package:webview_flutter/webview_flutter.dart';
+
+class PaymentPage extends StatefulWidget {
+  final String paymentUrl;
+
+  const PaymentPage({Key? key, required this.paymentUrl}) : super(key: key);
+
+  @override
+  State<PaymentPage> createState() => _PaymentPageState();
+}
+
+class _PaymentPageState extends State<PaymentPage> {
+  late final WebViewController controller;
+  bool isLoading = true;
+
+  @override
+  void initState() {
+    super.initState();
+    controller = WebViewController()
+      ..setJavaScriptMode(JavaScriptMode.unrestricted)
+      ..setNavigationDelegate(NavigationDelegate(
+        onPageFinished: (String url) {
+          setState(() {
+            isLoading = false;
+          });
+        },
+      ))
+      ..loadRequest(Uri.parse(widget.paymentUrl));
+  }
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: AppBar(
+        title: const Text('Payment'),
+        leading: IconButton(
+          icon: const Icon(Icons.close),
+          onPressed: () => Navigator.of(context).pop(),
+        ),
+      ),
+      body: Stack(
+        children: [
+          WebViewWidget(controller: controller),
+          if (isLoading)
+            const Center(
+              child: CircularProgressIndicator(),
+            ),
+        ],
+      ),
+    );
+  }
+}
